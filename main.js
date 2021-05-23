0bee8d1a44951932dec61c75153437fb4d08e67e
kritika-r <82035781+kritika-r@users.noreply.github.com>
2021-05-23 16:59:26 +0530

    keyevents-x&y axis- rover

--- main.js
+++ main.js
@@ -1,44 +1,93 @@
-var stud_array =[];
-function submit(){
-    var display_stud_array=[ ];
-    for (var j=1; j<5; j++){
-        var stud= document.getElementById("name_of_the_student_"+ j).value;
-        console.log(stud);
-        stud_array.push(stud);
-        
-    }
-    console.log(stud_array);
-    var length_of_stud_array= stud_array.length;
-    console.log(length_of_stud_array)
-    for( var k=0; k < length_of_stud_array; k++ ){
-        display_stud_array.push("<h4> NAME : "+stud_array[k]+"</h4>");
-        console.log(display_stud_array);
-    }
-    console.log(display_stud_array);
-    document.getElementById("display_name_with_commas").innerHTML= display_stud_array;
-    var remove_commas= display_stud_array.join(" ");
-    console.log(remove_commas);
-    document.getElementById("display_name_without_commas").innerHTML= remove_commas;
-    document.getElementById("submit_button").style.display= "none";
-    document.getElementById("sort_button").style.display= "inline-block";
-    
+canvas= document.getElementById('myCanvas');
+ctx= canvas.getContext("2d");
+
+rover_width= 100;
+rover_height= 90;
+
+rover_x= 10;
+rover_y= 10;
+
+background_img= "mars.jpg";
+rover_img= "rover.png";
+
+function add(){
+    bg_img_tag= new Image();
+    bg_img_tag.onload= upload_bg();
+    bg_img_tag.src= background_img;
+
+    rover_img_tag= new Image();
+    rover_img_tag.onload= upload_rover();
+    rover_img_tag.src= rover_img;
 
 }
-function sorting(){
-stud_array.sort();
-console.log(stud_array);
-var display_sorting=[];
-var length_of_stud_array= stud_array.length;
-    console.log(length_of_stud_array)
-    for( var k=0; k < length_of_stud_array; k++ ){
-        display_sorting.push("<h4> NAME : "+stud_array[k]+"</h4>");
-        console.log(display_sorting);
-    }
-  
-    var remove_commas= display_sorting.join(" ");
-    console.log(remove_commas);
-    document.getElementById("display_name_without_commas").innerHTML= remove_commas;
+function upload_bg(){
+    ctx.drawImage(bg_img_tag,0,0,canvas.width,canvas.height);
+
 }
-function update(){
-    document.getElementById("display_name_without_commas").innerHTML= "<h1>"+ stud_array+"</h1>"
+function upload_rover(){
+    ctx.drawImage(rover_img_tag,rover_x,rover_y,rover_width,rover_height);
+    
+}
+
+window.addEventListener("keydown", my_keydown);
+function my_keydown(e){
+keyPressed= e.keyCode;
+console.log(keyPressed);
+if(keyPressed=='38'){
+    up();
+    console.log("Up");
+
+}
+if(keyPressed=='40'){
+    down();
+    console.log("Down");
+    
+}
+if(keyPressed=='37'){
+    left();
+    console.log("Left");
+    
+}
+if(keyPressed=='39'){
+    right();
+    console.log("Right");
+    
+}
+
+}
+function up(){
+    if(rover_y>=0){
+        rover_y= rover_y-10;
+        console.log("When Up Arrow is pressed, X=" +rover_x+"Y=" +rover_y);
+        upload_bg();
+        upload_rover();
+
+    }
+}
+function down(){
+    if(rover_y<=500){
+        rover_y= rover_y+10;
+        console.log("When Down Arrow is pressed, X=" +rover_x+"Y=" +rover_y);
+        upload_bg();
+        upload_rover();
+        
+    }
+}
+function left(){
+    if(rover_x>=0){
+        rover_x= rover_x-10;
+        console.log("When Left Arrow is pressed, X=" +rover_x+"Y=" +rover_y);
+        upload_bg();
+        upload_rover();
+        
+    }
+}
+function right(){
+    if(rover_x<=700){
+        rover_x= rover_x+10;
+        console.log("When Right Arrow is pressed, X=" +rover_x+"Y=" +rover_y);
+        upload_bg();
+        upload_rover();
+        
+    }
 }
